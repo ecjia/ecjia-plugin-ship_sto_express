0e6affe92f81fd87ac1d507916c1e8616e911180
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-28 11:11:52 +0800

    更新配送方式插件

--- ship_sto_express.php
+++ ship_sto_express.php
@@ -84,6 +84,5 @@ Ecjia_PluginManager::extend('ship_sto_express', function() {
 
 RC_Plugin::register_activation_hook(__FILE__, array('plugin_ship_sto_express', 'install'));
 RC_Plugin::register_deactivation_hook(__FILE__, array('plugin_ship_sto_express', 'uninstall'));
-RC_Hook::add_filter('shipping_factory_adapter_instance', array( 'plugin_ship_sto_express', 'adapter_instance' ), 10, 2);
 
 // end
